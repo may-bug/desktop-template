b0f847c3b0739197a1fbe1c274c177729af4d944
lin <3186513948@qq.com>
2025-05-30 17:03:43 +0800

    remove other

--- desktop/src/renderer/src/models/IceCandidateMessage.ts
+++
@@ -1,8 +0,0 @@
-// src/models/IceCandidateMessage.ts
-import type { Message } from './Message'
-
-export interface IceCandidateMessage extends Message {
-  type: 'ice-candidate'
-  roomId: string
-  candidate: RTCIceCandidateInit
-}
--- desktop/src/renderer/src/models/SdpMessage.ts
+++
@@ -1,8 +0,0 @@
-// src/models/SdpMessage.ts
-import type { Message } from './Message'
-
-export interface SdpMessage extends Message {
-  type: 'offer' | 'answer'
-  roomId: string
-  sdp: RTCSessionDescriptionInit
-}
